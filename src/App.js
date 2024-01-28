c58b0665805ac0c4de171c86e5bc7a1971703ce5
David Copestakes <davecopestakes@gmail.com>
2024-01-28 16:52:32 -0500

    added testimonal text

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import BookingForm from './components/BookingForm';
 import Main from './components/Main';
 import Menu from './components/Menu';
+import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
 import logo from './images/Logo.svg';
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
         <Nav />
         <Main />
         <Menu />
+        <Testimonials />
         <Footer />
       </header>
     </>
---
+++ src/components/Testimonials.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Testimonials() {
+  return (
+    <section className="testimonial-container">
+      <h2>See what are Customers are Saying!</h2>
+      <div className="testimonial-card-container">
+        <div className="testimonial-card">
+          <h5>Great Food!</h5>
+          <p>
+            Little Lemon Restaurant is an absolute delight! The cozy ambiance,
+            coupled with an inventive menu, creates a culinary haven. Impeccable
+            service and a burst of flavors make every visit a memorable
+            experience.
+          </p>
+        </div>
+
+        <div className="testimonial-card">
+          <h5>Will be coming back</h5>
+          <p>
+            Little Lemon surpassed my expectations with its charming atmosphere
+            and delectable offerings. The chefs' creativity shines through in
+            every dish, making each visit a unique and delightful culinary
+            adventure.
+          </p>
+        </div>
+
+        <div className="testimonial-card">
+          <h5>Divine</h5>
+          <p>
+            Little Lemon is a culinary masterpiece! From the carefully curated
+            menu to the impeccable presentation, every detail is crafted to
+            perfection. The staff's passion for food and top-notch service make
+            dining here an exceptional experience.
+          </p>
+        </div>
+
+        <div className="testimonial-card">
+          <h5>A hidden gem</h5>
+          <p>
+            Little Lemon is a gem in the dining scene. The diverse menu,
+            blending unique flavors, and the welcoming ambiance create an
+            outstanding culinary journey. Fresh, locally sourced ingredients and
+            attentive staff make each visit to Little Lemon a consistently
+            excellent dining experience.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Testimonials;
