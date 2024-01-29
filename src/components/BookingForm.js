1dcc687e27f0df91b7535ec4b0d2bc02ca3ec1b0
David Copestakes <davecopestakes@gmail.com>
2024-01-29 18:23:56 -0500

    added some styles

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -20,64 +20,76 @@ function BookingForm(props) {
       <section>
         <form onSubmit={handleSubmit}>
           <fieldset className="formfield">
-            <div>
-              <label htmlFor="book-date">Choose Date:</label>
-              <input
-                id="book-date"
-                value={date}
-                onChange={(e) => handleChange(e.target.value)}
-                type="date"
-                required
-              />
-            </div>
-            {/* for time selection */}
-            <div>
-              <label htmlFor="book-time">Choose Time:</label>
-              <select
-                id="book-time"
-                value={times}
-                onChange={(e) => setTimes(e.target.value)}
-              >
-                <option value="">Select a Time</option>
-                {props.availableTimes.availableTimes.map((availableTimes) => {
-                  return <option key={availableTimes}>{availableTimes}</option>;
-                })}
-              </select>
-            </div>
-
-            {/* for number of guest */}
-            <div>
-              <label htmlFor="book-guests">Number of Guests:</label>
-              <input
-                id="book-guests"
-                min="1"
-                value={guests}
-                onChange={(e) => setGuest(e.target.value)}
-              />
-
-              {/* occasion field */}
+            <div className="formfield-flex">
+              {/*left colum */}
               <div>
-                <label htmlFor="book-occation">Occasion:</label>
-                <select
-                  id="book-occation"
-                  key={occasion}
-                  value={occasion}
-                  onChange={(e) => setOccasion(e.target.value)}
-                >
-                  <option>Birthday</option>
-                  <option>Anniversary</option>
-                </select>
+                <div>
+                  <label htmlFor="book-date">Choose Date:</label>
+                  <input
+                    id="book-date"
+                    value={date}
+                    onChange={(e) => handleChange(e.target.value)}
+                    type="date"
+                    required
+                  />
+                </div>
+                {/* for time selection */}
+                <div>
+                  <label htmlFor="book-time">Choose Time:</label>
+                  <select
+                    id="book-time"
+                    value={times}
+                    onChange={(e) => setTimes(e.target.value)}
+                  >
+                    <option value="">Select a Time</option>
+                    {props.availableTimes.availableTimes.map(
+                      (availableTimes) => {
+                        return (
+                          <option key={availableTimes}>{availableTimes}</option>
+                        );
+                      }
+                    )}
+                  </select>
+                </div>
               </div>
 
-              {/* submit button */}
-              <div className="btnReceive">
-                <input
-                  aria-label="On Click"
-                  type="submit"
-                  value={'Make Your Reservation'}
-                />
+              {/* for number of guest */}
+
+              {/* right column */}
+              <div>
+                <div>
+                  <label htmlFor="book-guests">Number of Guests:</label>
+                  <input
+                    id="book-guests"
+                    min="1"
+                    value={guests}
+                    onChange={(e) => setGuest(e.target.value)}
+                  />
+                </div>
+                {/* occasion field */}
+                <div>
+                  <label htmlFor="book-occation">Occasion:</label>
+                  <select
+                    id="book-occation"
+                    key={occasion}
+                    value={occasion}
+                    onChange={(e) => setOccasion(e.target.value)}
+                  >
+                    <option>Birthday</option>
+                    <option>Anniversary</option>
+                  </select>
+                </div>
               </div>
             </div>
+            {/* submit button */}
+            <div className="btnReceive">
+              <input
+                aria-label="On Click"
+                type="submit"
+                value={'Make Your Reservation'}
+              />
+            </div>
+            {/* </div> */}
           </fieldset>
         </form>
       </section>
