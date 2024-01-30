0c3f9a364c2fbb4ea3b77966d65ef60732327e5d
David Copestakes <davecopestakes@gmail.com>
2024-01-30 12:01:49 -0500

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Nav from './components/Nav';
-import Header from './components/Header';
-import BookingForm from './components/BookingForm';
 import Main from './components/Main';
 import Menu from './components/Menu';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
-import logo from './images/Logo.svg';
+// import logo from './images/Logo.svg';
 import './App.css';
 import About from './components/About';
 
