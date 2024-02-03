90e99f9bb360c4c80ced6765c4f1f7563d764125
David Copestakes <davecopestakes@gmail.com>
2024-02-03 08:53:08 -0500

    added Hashrouter

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+// change to hashrouter when you wake up tomorrow!!!!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <Router basename="/">
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 
