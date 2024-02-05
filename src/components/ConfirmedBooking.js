0804d63ba5528c5c683d7167853eb1cd939b0a2f
David Copestakes <davecopestakes@gmail.com>
2024-02-04 23:16:09 -0500

    added confirm image

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Header from './Header';
+import ConfirmImg from '../images/table-confirm.png';
+import BookingForm from './BookingForm';
 
 function ConfirmedBooking() {
   return (
     <div className="confirm">
       <div>
+        <img src={ConfirmImg} />
         <h1>
           Booking has been <span>Confirmed!</span>
         </h1>
       </div>
       <Link to="/">
-        <button>Return</button>
+        <button>Return to Menu</button>
       </Link>
     </div>
   );
